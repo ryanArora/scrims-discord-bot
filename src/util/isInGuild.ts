b9565c2bd3c099ff5f4b7b2d1644281b7c58ffd7
Ryan Arora <arora.ryan@gmail.com>
2021-08-02 08:19:36 -0400

    fix

--- src/util/isInGuild.ts
+++ src/util/isInGuild.ts
@@ -4,7 +4,7 @@ const isInGuild = async (uuid: string, guildId: string) => {
   const res = await axios.get(`https://api.hypixel.net/guild?key=${process.env.HYPIXEL_API_KEY}&id=${guildId}`).catch(() => {});
 
   const guild = res?.data;
-  if (!guild) return false;
+  if (!guild?.members) return false;
 
   for (const member of guild.members) {
     if (member.uuid === uuid) {
