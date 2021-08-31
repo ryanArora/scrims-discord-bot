8cfc325199db2c3c5e1a8a9931075a504dd7da89
Ryan Arora <arora.ryan@gmail.com>
2021-08-31 04:00:15 -0400

    fix

--- src/schemas/Player.ts
+++ src/schemas/Player.ts
@@ -37,7 +37,7 @@ export interface IPlayer extends Document {
 
 const PlayerSchema = new Schema({
   discordId: { type: String, required: true, index: true, unique: true },
-  guildId: { type: String, required: true, default: null },
+  guildId: { type: String, default: null },
   name: { type: String, requried: true, index: true, unique: true },
   nickname: String,
   uuid: { type: String, requried: true, unique: true },
